3272e43e534c9108c5eda997eb456c2051e2041c
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2020-07-17 16:10:39 +0800

    Make this plugin compatible with blueprint
    Please note that, this commit has not been tested yet. If test being conducted later fail, this commit may be reverted.

--- flask_hcaptcha.py
+++ flask_hcaptcha.py
@@ -1,10 +1,9 @@
 """
-The new Google ReCaptcha implementation for Flask without Flask-WTF
-Can be used as standalone
+A hCaptcha extension for Flask based on flask-recaptcha 
 """
 
 __NAME__ = "Flask-hCaptcha"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __license__ = "MIT"
 __author__ = "Knugi (originally ReCaptcha by Mardix)"
 __copyright__ = "(c) 2020 Knugi (originally ReCaptcha by Mardix 2015)"
@@ -16,6 +15,9 @@ try:
 except ImportError as ex:
     print("Missing dependencies")
 
+class BlueprintCompatibility(object):
+    site_key = None
+    secret_key = None
 
 class DEFAULTS(object):
     IS_ENABLED = True
@@ -29,8 +31,8 @@ class hCaptcha(object):
 
     def __init__(self, app=None, site_key=None, secret_key=None, is_enabled=True, **kwargs):
         if site_key:
-            self.site_key = site_key
-            self.secret_key = secret_key
+            BlueprintCompatibility.site_key = site_key
+            BlueprintCompatibility.secret_key = secret_key
             self.is_enabled = is_enabled
 
         elif app:
@@ -53,12 +55,12 @@ class hCaptcha(object):
         return "" if not self.is_enabled else ("""
         <script src="https://hcaptcha.com/1/api.js" async defer></script>
         <div class="h-captcha" data-sitekey="{SITE_KEY}"></div>
-        """.format(SITE_KEY=self.site_key))
+        """.format(SITE_KEY=BlueprintCompatibility.site_key))
 
     def verify(self, response=None, remote_ip=None):
         if self.is_enabled:
             data = {
-                "secret": self.secret_key,
+                "secret": BlueprintCompatibility.secret_key,
                 "response": response or request.form.get('h-recaptcha-response'),
                 "remoteip": remote_ip or request.environ.get('REMOTE_ADDR')
             }

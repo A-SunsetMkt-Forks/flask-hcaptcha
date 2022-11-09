6d715f3f44dd0d269946c91e4de3e23eac0e5ce1
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2022-11-09 23:22:40 +0800

    Add quart compatibility
    https://github.com/KnugiHK/flask-hcaptcha/issues/7

--- flask_hcaptcha.py
+++ flask_hcaptcha.py
@@ -8,22 +8,47 @@ __license__ = "MIT"
 __author__ = "Knugi (originally ReCaptcha by Mardix)"
 __copyright__ = "(c) 2020 Knugi (originally ReCaptcha by Mardix 2015)"
 
+flask_available = False
+quart_available = False
+request = None
+
 try:
-    from flask import request
+    try:
+        from flask import request as flask_request
+    except ImportError:
+        ...
+    else:
+        flask_available = True
+
+    try:
+        from quart import request as quart_request
+    except ImportError:
+        ...
+    else:
+        quart_available = True
+
+    if not flask_available and not quart_available:
+        raise ImportError
+
     try:
         from jinja2 import Markup
     except ImportError:
         from markupsafe import Markup
     import requests
-except ImportError as ex:
-    print("Missing dependencies")
+except ImportError:
+    print("flask_hcaptcha: Missing dependencies")
+    exit()
+
 
 class BlueprintCompatibility(object):
     site_key = None
     secret_key = None
 
+
 class DEFAULTS(object):
     IS_ENABLED = True
+    ASYNC = False
+
 
 class hCaptcha(object):
 
@@ -45,6 +70,21 @@ class hCaptcha(object):
         self.__init__(site_key=app.config.get("HCAPTCHA_SITE_KEY"),
                       secret_key=app.config.get("HCAPTCHA_SECRET_KEY"),
                       is_enabled=app.config.get("HCAPTCHA_ENABLED", DEFAULTS.IS_ENABLED))
+        global request
+        if app.config.get("HCAPTCHA_ASYNC", DEFAULTS.ASYNC):
+            self.verify = self.verify_async
+            try:
+                request = quart_request
+            except NameError:
+                print("flask_hcaptcha: Missing dependencies. Did you accidentally set HCAPTCHA_ASYNC to True?")
+                exit()
+        else:
+            self.verify = self.verify_sync
+            try:
+                request = flask_request
+            except NameError:
+                print("flask_hcaptcha: Missing dependencies")
+                exit()
 
         @app.context_processor
         def get_code():
@@ -60,12 +100,23 @@ class hCaptcha(object):
         <div class="h-captcha" data-sitekey="{SITE_KEY}"></div>
         """.format(SITE_KEY=BlueprintCompatibility.site_key))
 
-    def verify(self, response=None, remote_ip=None):
+    def verify_sync(self, response=None, remote_ip=None):
         if self.is_enabled:
             data = {
                 "secret": BlueprintCompatibility.secret_key,
                 "response": response or request.form.get('h-captcha-response'),
                 "remoteip": remote_ip or request.environ.get('REMOTE_ADDR')
+            }
+
+            r = requests.post(self.VERIFY_URL, data=data)
+            return r.json()["success"] if r.status_code == 200 else False
+        return True
+
+    async def verify_async(self, response=None):
+        if self.is_enabled:
+            data = {
+                "secret": BlueprintCompatibility.secret_key,
+                "response": response or (await request.form).get('h-captcha-response', ""),
             }
 
             r = requests.post(self.VERIFY_URL, data=data)
